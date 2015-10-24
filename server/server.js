c39824c6afdcbc672d7257099e844db972419b9f
WajidKagzi <wjkagzi@gmail.com>
2015-10-23 22:01:13 -0400

    Lets add some middleware and routing.

--- server/server.js
+++ server/server.js
@@ -20,3 +20,25 @@ app.all('/*', function(req, res, next) {
     next();
   }
 });
+
+// Auth Middleware - This will check if the token is valid
+// Only the requests that start with /api/v1/* will be checked for the token.
+// Any URL's that do not follow the below pattern should be avoided unless you
+// are sure that authentication is not needed
+app.all('/api/v1/*', [require('./middlewares/validateRequest')]);
+
+app.use('/', require('./routes'));
+
+// If no route is matched by now, it must be a 404
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Start the server
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
